855c29fab71cceaf9b52798db8d382418a0150b7
heet bhalodiya <heetbhalodiya007@gmail.com>
2024-09-06 10:47:11 +0530

    add tinacms

---
+++ CHANGELOG.md
@@ -0,0 +1,29 @@
+# next-2024
+
+## 0.1.3
+
+### Patch Changes
+
+- Updated dependencies [6ccda6c]
+- Updated dependencies [33eaa81]
+- Updated dependencies [f59d67b]
+- Updated dependencies [daeeebf]
+- Updated dependencies [27bfe84]
+  - tinacms@2.2.2
+
+## 0.1.2
+
+### Patch Changes
+
+- Updated dependencies [ae03e8e]
+- Updated dependencies [4c9f221]
+  - tinacms@2.2.1
+
+## 0.1.1
+
+### Patch Changes
+
+- Updated dependencies [324950a]
+- Updated dependencies [f378f11]
+- Updated dependencies [ceb0c07]
+  - tinacms@2.2.0
---
+++ app/posts/page.tsx
@@ -0,0 +1,41 @@
+import client from "@/tina/__generated__/client";
+import Link from "next/link";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
+
+export default async function Page() {
+  const { data: posts } = await client.queries.postConnection();
+  const { data } = await client.queries.articleConnection();
+
+  return (
+    <>
+      <h1 className="text-2xl mt-4">Posts</h1>
+      <div>
+        {posts.postConnection.edges?.map((post) => (
+          <div key={post?.node?.id}>
+            <Link href={`/posts/${post?.node?._sys.filename}`}>
+              {post?.node?._sys.filename}
+            </Link>
+          </div>
+        ))}
+      </div>
+      <h1 className="text-2xl mt-4">Articles</h1>
+      <div>
+        {data.articleConnection.edges?.map((article) => (
+          <div
+            key={article?.node?.id}
+            className="border rounded m-2 p-5 bg-slate-700"
+          >
+            <p className="text-xl font-bold">{article?.node?.title}</p>
+            {article?.node?.blocks?.map((block) => (
+              <div>
+                {block?.description.children && (
+                  <TinaMarkdown content={block.description} />
+                )}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
